e43ccf5a2f5344a88135b292aa292d747c162c5a
AntonSP-web <65166212+AntonSP-web@users.noreply.github.com>
2022-03-14 19:16:14 +0300

    fix: change sidebar block

---
+++ sidebar.js
@@ -0,0 +1,21 @@
+const buttonArrow = document.querySelector('.sidebar__button');
+const sideBarList = document.querySelector('.sidebar__list');
+
+
+sideBarList.addEventListener('click', (evt) => {
+  const elem = evt.target.closest('.sidebar__item-wrap');
+  console.log(elem);
+  const listActivity = elem.querySelector('.sidebar__list-activity');
+  console.log(listActivity)
+
+  if(evt.target.classList.contains('sidebar__button_type_arrow-down')) {
+    listActivity.classList.remove('sidebar__list-activity_closed');
+    evt.target.classList.remove('sidebar__button_type_arrow-down');
+    evt.target.classList.add('sidebar__button_type_arrow-up');
+
+  } else if(evt.target.classList.contains('sidebar__button_type_arrow-up')) {
+    evt.target.classList.remove('sidebar__button_type_arrow-up');
+    evt.target.classList.add('sidebar__button_type_arrow-down');
+    listActivity.classList.add('sidebar__list-activity_closed');
+  }
+});
