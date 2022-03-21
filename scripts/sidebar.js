fa2cbb6f56cde0cc3dd6eb8c580901f0a13a8245
AntonSP-web <65166212+AntonSP-web@users.noreply.github.com>
2022-03-21 18:14:47 +0300

    fix: add eventListener to whole item instead of button only aside

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,17 +1,29 @@
-const buttonArrow = document.querySelector('.sidebar__button');
-const sideBarList = document.querySelector('.sidebar__list');
+// const buttonArrow = document.querySelector('.sidebar__button');
+// const sideBarList = document.querySelector('.sidebar__list');
 
-sideBarList.addEventListener('click', (evt) => {
-  const elem = evt.target.closest('.sidebar__item');
-  const listActivity = elem.querySelector('.sidebar__list-activity');
-  if(evt.target.classList.contains('sidebar__button_type_arrow-down')) {
-    listActivity.classList.remove('sidebar__list-activity_closed');
-    evt.target.classList.remove('sidebar__button_type_arrow-down');
-    evt.target.classList.add('sidebar__button_type_arrow-up');
+// sideBarList.addEventListener('click', (evt) => {
+//   const elem = evt.target.closest('.sidebar__item');
+//   const listActivity = elem.querySelector('.sidebar__list-activity');
+//   if(evt.target.classList.contains('sidebar__button_type_arrow-down')) {
+//     listActivity.classList.remove('sidebar__list-activity_closed');
+//     evt.target.classList.remove('sidebar__button_type_arrow-down');
+//     evt.target.classList.add('sidebar__button_type_arrow-up');
 
-  } else if(evt.target.classList.contains('sidebar__button_type_arrow-up')) {
-    evt.target.classList.remove('sidebar__button_type_arrow-up');
-    evt.target.classList.add('sidebar__button_type_arrow-down');
-    listActivity.classList.add('sidebar__list-activity_closed');
-  }
-});
+//   } else if(evt.target.classList.contains('sidebar__button_type_arrow-up')) {
+//     evt.target.classList.remove('sidebar__button_type_arrow-up');
+//     evt.target.classList.add('sidebar__button_type_arrow-down');
+//     listActivity.classList.add('sidebar__list-activity_closed');
+//   }
+// });
+
+const sidebarItems = document.querySelectorAll('.sidebar__item');
+
+sidebarItems.forEach(item => {
+  item.addEventListener('click', () => {
+    const listActivity = item.querySelector('.sidebar__list-activity');
+    const buttonArrow = item.querySelector('.sidebar__button');
+    buttonArrow.classList.toggle('sidebar__button_type_arrow-up');
+    buttonArrow.classList.toggle('sidebar__button_type_arrow-down');
+    listActivity.classList.toggle('sidebar__list-activity_closed');
+  })
+})
